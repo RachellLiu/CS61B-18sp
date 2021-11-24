17d49d70898fcdab43da72a6dbeb631f9639a362
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2021-11-24 20:50:29 +0000

    LinkedDeque

---
+++ proj1a/ArrayTest.java
@@ -0,0 +1,14 @@
+/** Performs some basic linked list tests. */
+public class ArrayTest {
+	public static void main(String[] args) {
+		ArrayDeque<Integer> lld1 = new ArrayDeque<Integer>();
+		//lld1.addFirst(10);
+		//lld1.addFirst(1);
+		//lld1.addLast(100);
+		//lld1.removeFirst();
+		//lld1.removeLast();
+		lld1.printDeque();
+
+		System.out.println(lld1.isEmpty());
+	}
+}
