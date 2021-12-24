22975b046927871b9d71e16f3d779b36ba7334f4
Theta42 <26573482+RachellLiu@users.noreply.github.com>
2021-12-24 00:07:04 +0000

    proj1b

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,38 @@
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word){
+        ArrayDeque<Character> wordDeque = new ArrayDeque<>();
+        for (char ch: word.toCharArray()){
+            wordDeque.addLast(ch);
+        }
+        return wordDeque;
+    }
+
+    public boolean isPalindrome(String word){
+        Deque<Character> wordDeque = wordToDeque(word);
+        return isPalindrome_helper(wordDeque);
+    }
+
+    public boolean isPalindrome_helper(Deque<Character> wordDeque){
+        if (wordDeque.size() <= 1) {
+            return true;
+        }
+        else if (wordDeque.removeFirst() == wordDeque.removeLast()) {
+            return isPalindrome_helper(wordDeque);
+        }
+        return false;
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        Deque<Character> wordDeque = wordToDeque(word);
+        return isPalindrome_helper(wordDeque, cc);
+    }
+
+    public boolean isPalindrome_helper(Deque<Character> wordDeque, CharacterComparator cc) {
+        if (wordDeque.size() <= 1) {
+            return true;
+        }
+        char first = wordDeque.removeFirst();
+        char last = wordDeque.removeLast();
+        return (cc.equalChars(first, last) && isPalindrome_helper(wordDeque, cc));
+    }
+}
